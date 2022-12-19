c2e80840a1fdb4dbdce0af50cec02a5aed2a96e1
Mammadli999 <abbas.memmedli.1990@mail.ru>
2022-12-19 23:38:08 +0400

    Asp Final

---
+++ Cv.Domain/Models/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Cv.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv.Domain.Models.ViewModels
+{
+    public class PortfolioViewModel
+    {
+        public ICollection<ProjectCategory> ProjectCategories { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
